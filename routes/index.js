831a45d86af988b777a4b695e9f59411b4a4dce1
jadersonmarc@gmail.com <jadersonmarc@gmail.com>
2019-12-23 22:26:44 -0300

    using include in route menus

--- routes/index.js
+++ routes/index.js
@@ -29,20 +29,15 @@ router.get('/contacts', function(req, res, next) {
 
 router.get('/menus', function(req, res, next) {
 
-  conn.query("SELECT * FROM tb_menus ORDER BY title", (err, results) => {
-
-    if(err){
-     console.log(err);
-    }
+  menus.getMenus().then(results => {
 
     res.render('index', { 
       title: 'Restaurante Saboroso!',
       menus: results ,
       h1: 'Saboreie nosso menu!'
     });
-  });
 
-  
+  });
 
 });
 
