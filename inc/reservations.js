6480346716a1697b34f6d74d2de0c79878aae691
jadersonmarc@gmail.com <jadersonmarc@gmail.com>
2020-01-25 18:41:22 -0300

    add data error on render

--- inc/reservations.js
+++ inc/reservations.js
@@ -1,10 +1,11 @@
 module.exports = {
-  render(req, res) {
+  render(req, res, error) {
     res.render('reservations', {
       title: 'Restaurante Saboroso!',
       background: 'images/img_bg_2.jpg',
       h1: 'Reserve uma Mesa!',
-      body: req.boby
+      body: req.boby,
+      error
     });
 
 
