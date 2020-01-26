3f3cca065bbe76a8197270cfdb06f68f38b0233e
jadersonmarc@gmail.com <jadersonmarc@gmail.com>
2020-01-25 21:59:14 -0300

    create post contact

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var conn = require('./../inc/db');
 var menus = require('./../inc/menus');
 var reservations = require('./../inc/reservations');
+var contacts = require('./../inc/contacts');
 var express = require('express');
 var router = express.Router();
 
@@ -21,11 +22,26 @@ router.get('/', function (req, res, next) {
 
 router.get('/contacts', function (req, res, next) {
 
-  res.render('contacts', {
-    title: 'Restaurante Saboroso!',
-    background: 'images/img_bg_3.jpg',
-    h1: 'Diga um oi!'
-  });
+  contacts.render(req, res);
+
+});
+
+router.post('/contacts', function (req, res, next) {
+
+  if (!req.body.name) {
+    contacts.render(req, res, 'digite o nome');
+  } else if (!req.body.email) {
+    contacts.render(req, res, 'digite o email');
+  } else if (!req.body.message) {
+    contacts.render(req, res, 'deixe uma mensagen');
+  } else {
+    contacts.save(req.body).then(results => {
+      req.body = {};
+      contacts.render(req, res, null, 'contato realizado com sucesso!');
+    }).catch(err => {
+      contacts.render(req, res, err.message);
+    })
+  }
 
 });
 
